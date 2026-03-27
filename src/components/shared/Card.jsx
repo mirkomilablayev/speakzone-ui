4ad3f667f6b2c707493b0562a8ee1232681e2181
ablayev1999 <ablayev1999@gmail.com>
2026-03-27 15:02:50 +0500

    fix: enable onClick and other props for Card component

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,7 +1,8 @@
-export default function Card({ children, className = "", padding = "p-4" }) {
+export default function Card({ children, className = "", padding = "p-4", ...props }) {
   return (
     <div
       className={`bg-card card-border rounded-xl2 shadow-card ${padding} ${className}`}
+      {...props}
     >
       {children}
     </div>
